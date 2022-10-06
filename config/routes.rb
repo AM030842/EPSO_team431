ec001cf498c9097edc052053ffd13e288a108805
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-10-06 16:18:48 -0500

    reinstalled webpacker to get rid "webpacker nf"

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   resources :alums
   resources :meetings
   resources :attendances
-  resources :members
-  resources :logs
   resources :meetings
-  root 'alumnis#index'
+  root 'meetings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
