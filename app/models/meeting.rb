4c8e5a375449b67dadc20c06752f7af425a3b561
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-10-07 08:47:50 -0500

    addded relationships to the models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,7 @@
 class Meeting < ApplicationRecord
+    has_many :members, through :members
+    has_many :alums, through :alums
+    has_many :attendances
+
+    validates :meeting_title, :meeting_time, :meeting_desc: true
 end
