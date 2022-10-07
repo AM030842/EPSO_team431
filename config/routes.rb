39aad4b2b9a2a87f9ada9b7dbca6bbab7b71fbc4
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-10-07 09:13:18 -0500

    got rid of duplicate meetings in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :alums
   resources :meetings
   resources :attendances
-  resources :meetings
   root 'meetings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
