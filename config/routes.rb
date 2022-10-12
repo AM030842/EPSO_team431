7ebe2adeda63923f4480e3fe74aefb3a3f4aca56
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-10-11 22:01:26 -0500

    connecting member and meeting to attendance entity

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :alums
   resources :meetings
   resources :attendances
+<<<<<<< Updated upstream
   resources :meetings
   root 'meetings#index'
+=======
+  root 'attendances#index'
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
