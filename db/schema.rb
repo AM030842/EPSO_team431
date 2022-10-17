c86c365d223e663c34a3f2f07a6e2addce09fb71
Depasinre <44266228+Depasinre@users.noreply.github.com>
2022-10-17 14:41:16 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -59,5 +59,4 @@ ActiveRecord::Schema.define(version: 2022_10_13_165358) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
