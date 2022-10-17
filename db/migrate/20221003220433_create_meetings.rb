4a607cb8a62f640b4121520d1c8160bd9720b6fb
Depasinre <44266228+Depasinre@users.noreply.github.com>
2022-10-17 15:33:59 -0500

    Create 20221003220433_create_meetings.rb

---
+++ db/migrate/20221003220433_create_meetings.rb
@@ -0,0 +1,13 @@
+class CreateMeetings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meetings do |t|
+      t.integer :meeting_id
+      t.integer :meeting_pts
+      t.string :meeting_title
+      t.datetime :meeting_time
+      t.text :meeting_desc
+
+      t.timestamps
+    end
+  end
+end
