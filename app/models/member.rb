0c499c0ce8258dee99ffa30ed1f8986b434bf447
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-11-08 11:31:16 -0600

    added "validates" to appRecord for error checking

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,8 @@
 class Member < ApplicationRecord
-    has_many :meetings, through: :meetings
+
+    has_many :meetings, through: :attendances
+    has_many :attendances
+
+    validates :name, :email, :points, presence: true
+
 end
