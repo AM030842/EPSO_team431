c466d37a07883de5f0636d5da45e389c5f5c5632
Depasinre <44266228+Depasinre@users.noreply.github.com>
2022-11-28 17:09:53 -0600

    add destroy dependencies

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
     rolify
 
     has_many :meetings, through: :attendances
-    has_many :attendances
+    has_many :attendances, dependent: :destroy
     validates :name, :email, presence: true
     validates_uniqueness_of :email
 
